66a822e2b75edf5701d88ee0e505841e9666ca34
donkennie <ajeigbekehinde160@gmail.com>
2023-06-15 21:12:34 +0100

    Add project files.

---
+++ Serverless_CRUD_api/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serverless_CRUD_api
+{
+    internal class Product
+    {
+    }
+}
